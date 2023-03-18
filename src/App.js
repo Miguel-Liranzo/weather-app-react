c557a79b8c718db61c110159900b8dcec10bc998
Miguel-Liranzo <94998643+Miguel-Liranzo@users.noreply.github.com>
2023-03-18 12:57:28 -0400

    first commit.layout and styling finished. missing functionality.

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5a5211c083d316c666d1d742db2831b3`;
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+    }
+  }
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event  => setLocation(event.target.value)}
+        onKeyUp={searchLocation}
+        placeholder='Enter Location'
+        type="text"/>
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Yonkers</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">60°F</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
